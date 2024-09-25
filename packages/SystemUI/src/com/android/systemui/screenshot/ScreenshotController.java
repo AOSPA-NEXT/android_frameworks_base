1d954b011892630440a0b9c08b7598f8a4fe4f0e
Ido Ben-Hur <idoybh2@gmail.com>
2024-09-25 21:28:11 +0300

    SystemUI: screenshot: Fixup notification and re-add the delete action
    Change-Id: I683bacaf9f45e61c76789f5432cc3db0b13f4ce9
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -943,6 +943,8 @@ public class ScreenshotController implements ScreenshotHandler {
                 if (result.uri != null) {
                     mActionsController.setCompletedScreenshot(requestId, new ScreenshotSavedResult(
                             result.uri, screenshot.getUserOrDefault(), result.timestamp));
+                    mNotificationsController.showPostActionNotification(
+                            result.uri, mScreenBitmap);
                 }
                 if (DEBUG_CALLBACK) {
                     Log.d(TAG, "finished background processing, Calling (Consumer<Uri>) "
@@ -1016,8 +1018,6 @@ public class ScreenshotController implements ScreenshotHandler {
                 } else {
                     mViewProxy.setChipIntents(imageData);
                 }
-                mNotificationsController.showPostActionNotification(
-                    imageData, mScreenBitmap);
             });
         }
     }
